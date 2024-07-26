8bc2bc640810e80182d17eddab08e85023403929
hlmandy <huanglei.mandy@gmail.com>
2024-07-26 13:46:42 +0800

    eng_name->ch_name

--- docs/_sidebar.md
+++ docs/_sidebar.md
@@ -5,19 +5,17 @@
   * [GitBash](/01_GitTutorial/02_GitBash.md)
   * [Sourcetree](/01_GitTutorial/03_Sourcetree.md)
   * [VSCode](/01_GitTutorial/04_VSCode.md)
-  * [Sourcetree](/01_GitTutorial/Sourcetree.md)
 * WritingTools
-  * [Obsidian](/02_WritingTools/01_Obsidian/01_Obsidian.md)
-    * [Plugin_Excalidraw](/02_WritingTools/01_Obsidian/Plugin_Excalidraw.md)
+  * Obsidian
+    * [Obsidian七七八八](/02_WritingTools/01_Obsidian/01_Obsidian_collect.md)
+    * [画图插件_Excalidraw](/02_WritingTools/01_Obsidian/Plugin_Excalidraw.md)
   * [Latex](/02_WritingTools/02_Latex.md)
   * [Markdown_test](/02_WritingTools/09_Markdown_test.md)
-* coding
 * opti_solver
   * [CPLEX_C++](/04_opti_solver/CPLEX_C++.md)
   * [CPLEX_doc_setting](/04_opti_solver/CPLEX_doc_setting.md)
   * [DOCplex](/04_opti_solver/DOCplex.md)
 * mandy_prob_solve
-  * [windows_context_modify](/88_mandy_prob_solve/windows_context_modify.md)
-* mandy_devices
-  * [Keychron_Q5_Setting](/99_mandy_devices/Keychron_Q5_Setting.md)
-* [home](/home.md)
+  * [windows右键菜单更改](/88_mandy_prob_solve/windows_context_modify.md)
+* mandy的设备配置
+  * [Keychron Q5 自定义按键](/99_mandy_devices/Keychron_Q5_Setting.md)
--- generate_sidebar.py
+++ generate_sidebar.py
@@ -4,26 +4,30 @@ import json
 def generate_sidebar(dir_path, output_file):
     def traverse_directory(current_path, indent_level):
         sidebar_contents = []
+        items_in_current_path = os.listdir(current_path)
+        chn_rename_dict = {}
+        if '_chn_rename.json' in items_in_current_path:
+            with open(os.path.join(current_path, '_chn_rename.json'), 'r', encoding='utf-8') as f:
+                chn_rename_dict = json.load(f)
         for item in sorted(os.listdir(current_path)):
             if item.startswith('_') or item.startswith('.'):
                 continue
             item_path = os.path.join(current_path, item)
             item_caption = get_name(item)
-            print("item:",item)
-            print("\titem_path:",item_path)
-            print("\titem_caption:",item_caption)
-            if os.path.isdir(item_path):
+
+            item_alias = chn_rename_dict.get(item_caption, item_caption)
+            if os.path.isdir(item_path):  ## -- 如果是文件夹
                 if has_same_name_in_folder(item_path):
                     relative_path = os.path.relpath(item_path, dir_path).replace("\\", "/")
-                    sidebar_contents.append('  ' * indent_level + f"* [{item_caption}](/{relative_path}/{item}.md)\n")
+                    sidebar_contents.append('  ' * indent_level + f"* [{item_alias}](/{relative_path}/{item}.md)\n")
                     # sidebar_contents.append('  ' * indent_level + f"* [{item_caption}]({dir_path.replace("\\", "/")}/{item}.md)\n")
                 else:
-                    sidebar_contents.append('  ' * indent_level + f"* {item_caption}\n")
+                    sidebar_contents.append('  ' * indent_level + f"* {item_alias}\n")
                 sidebar_contents+=traverse_directory(item_path, indent_level + 1)
             elif item.endswith('.md'):
                 if get_name(item) != get_name(current_path):
                     relative_path = os.path.relpath(item_path, dir_path).replace("\\", "/")
-                    sidebar_contents.append('  ' * indent_level + f"* [{item_caption}](/{relative_path})\n")
+                    sidebar_contents.append('  ' * indent_level + f"* [{item_alias}](/{relative_path})\n")
                     # sidebar_contents.append('  ' * indent_level + f"* [{item_caption}]({dir_path.replace("\\", "/")})\n")
         return sidebar_contents
 
@@ -38,16 +42,7 @@ def has_same_name_in_folder(folder_path):
 
 def get_name(filepath):
     filename = os.path.basename(filepath)
-    filedir = os.path.dirname(filepath)
     fullname = os.path.splitext(filename)[0]
-    # filedir中是否存在_chn_rename.json文件，如果存在，则取其中的name值作为文件名, ## utf-8编码
-    print("path:",filepath,"dir:",filedir)
-    if os.path.exists(os.path.join(filedir, '_chn_rename.json')): 
-        print("_chn_rename:",os.path.join(filedir, '_chn_rename.json'))
-        with open(os.path.join(filedir, '_chn_rename.json'), 'r', encoding='utf-8') as f:
-            chn_rename_dict = json.load(f)
-        if fullname in chn_rename_dict:
-            fullname = chn_rename_dict[fullname]
     ## 如果_前面有数字编号，则取[1:]
     if '_' in fullname:
         first_part = fullname.split('_')[0]
